f9ceeb810e0f812f4c8aa2d7d1b4383c57f71899
johnhealycodes <johndhealy1216@gmail.com>
2023-04-07 18:01:34 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -17,3 +17,16 @@ let areYouPlayingBanjo = (name) => name[0] === 'r' || name[0] === 'R' ? `${name}
 // findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
 
 const findUniq = (arr) => arr.find(v => arr.indexOf(v) === arr.lastIndexOf(v))
+
+
+// Given a string of words, you need to find the highest scoring word.
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+// For example, the score of abad is 8 (1 + 2 + 1 + 4).
+// You need to return the highest scoring word as a string.
+// If two words score the same, return the word that appears earliest in the original string.
+// All letters will be lowercase and all inputs will be valid.
+
+function high(s){
+    let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+    return s.split(' ')[as.indexOf(Math.max(...as))];
+  }

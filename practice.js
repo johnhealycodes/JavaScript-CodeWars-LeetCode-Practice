4f39dcf9e3f5ee041796a933d88fd20eaf1130a2
johnhealycodes <johndhealy1216@gmail.com>
2023-04-08 23:25:48 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -30,3 +30,28 @@ function high(s){
     let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
     return s.split(' ')[as.indexOf(Math.max(...as))];
   }
+
+  // Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+  // If you want to know more: http://en.wikipedia.org/wiki/DNA  
+  // In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+  //Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. 
+  //DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+  // More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+  // Example: (input --> output)  
+  // "ATTGC" --> "TAACG"
+  // "GTAT" --> "CATA"
+
+  let DNAStrand = (dna) => {
+    let arr = dna.split("")
+    return arr.map(i => {
+      if (i === "A"){
+        return "T"
+      } else if (i === "T"){
+        return "A"
+      } else if (i === "G"){
+        return "C"
+      } else {
+        return "G"
+      }
+    }).join("")
+  }

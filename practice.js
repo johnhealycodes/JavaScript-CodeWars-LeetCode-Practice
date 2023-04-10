bf10cccb47384c45f19d104f8e9135e9bdf15c0c
johnhealycodes <johndhealy1216@gmail.com>
2023-04-09 19:54:05 -0500

    Logarithm algorithm

--- practice.js
+++ practice.js
@@ -55,3 +55,12 @@ function high(s){
       }
     }).join("")
   }
+
+
+// Complete the function power_of_two/powerOfTwo (or equivalent, 
+//depending on your language) that determines if a given non-negative integer is a power of two. 
+//From the corresponding Wikipedia entry:
+//a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation
+//with number two as the base and integer n as the exponent.
+
+const isPowerOfTwo = (n) => Number.isInteger(Math.log2(n))

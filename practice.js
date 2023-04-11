787b415fad3cd8f849e94745f2c9e1c1d10c2df4
johnhealycodes <johndhealy1216@gmail.com>
2023-04-10 19:49:49 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -91,3 +91,11 @@ const quarterOf = (month) => {
     return 4
   }
 }
+
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+const minMax = (arr) => {
+  let sorted = arr.sort((a, b) => a - b)
+  let result = [sorted[0], sorted[sorted.length - 1]]
+  return result
+}

0fa4f8f09bf36c7d27a5ce2fa9887e6017ffa7d0
johnhealycodes <johndhealy1216@gmail.com>
2023-04-10 21:46:50 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -99,3 +99,14 @@ const minMax = (arr) => {
   let result = [sorted[0], sorted[sorted.length - 1]]
   return result
 }
+
+//Write a function that accepts an array of 10 integers (between 0 and 9),
+//that returns a string of those numbers in the form of a phone number.
+
+const createPhoneNumber = (numbers) => {
+  let areaCode = numbers.splice(0, 3).join("")
+  let middle = numbers.splice(0, 3).join("")
+  let end = numbers.splice(0, 4).join("")
+  
+  return `(${areaCode}) ${middle}-${end}`
+}

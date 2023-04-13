2327835b75794987b72dfb732cee5c03f95e79d4
johnhealycodes <johndhealy1216@gmail.com>
2023-04-13 16:58:31 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -148,3 +148,11 @@ const printerError = (s) => {
 // accum("abcd") -> "A-Bb-Ccc-Dddd"
 
 const accum = (s) => s.split("").map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-')
+
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+const findShort = (s) => {
+  let a = s.split(" ").sort((a, b) => a.length - b.length)
+  return a[0].length
+}

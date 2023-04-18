3892fbbfacc1b3c827aad9e069970e0cc1d4d011
johnhealycodes <johndhealy1216@gmail.com>
2023-04-18 16:25:15 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -211,3 +211,7 @@ const highAndLow = (n) => {
   let result = n.split(" ").sort((a, b) => a - b)
   return `${result[result.length - 1]} ${result[0]}`
  }
+
+//  Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+const sumMix = (x) => x.map(i => Number(i)).reduce((a,c) => a + c, 0)

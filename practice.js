3aa7d05dccdf62e1156747c76fb0d95f8b989e3a
johnhealycodes <johndhealy1216@gmail.com>
2023-04-24 16:27:43 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -284,3 +284,20 @@ const squareDigits =(n) =>{
 // Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
 
 const flattenAndSort = (a) => [].concat(...a).sort((a,b) => a - b)
+
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+function getGrade (s1, s2, s3) {
+  let grade = (s1 + s2 + s3) / 3
+  if (grade >= 90){
+    return 'A'
+  } else if (grade >= 80 && grade < 90){
+    return 'B'
+  } else if (grade >= 70 && grade < 80){
+    return 'C'
+  } else if (grade >= 60 && grade < 70){
+    return 'D'
+  } else {
+    return 'F'
+  }
+}

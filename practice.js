03a16b0de358672219ce7def03d44aa85250726e
johnhealycodes <johndhealy1216@gmail.com>
2023-04-25 19:46:51 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -378,3 +378,9 @@ const fruitInspection = async() => {
     console.log('Looks peachy!')
   }
 }
+
+// Given an integral number, determine if it's a square number:
+
+const isSquare = function(n){
+  return Math.sqrt(n) % 1 === 0
+}

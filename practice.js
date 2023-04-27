6710d44c1d725c11354f959dedc7101cdb8ab809
johnhealycodes <johndhealy1216@gmail.com>
2023-04-26 19:28:23 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -384,3 +384,19 @@ const fruitInspection = async() => {
 const isSquare = function(n){
   return Math.sqrt(n) % 1 === 0
 }
+
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. 
+// Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+const diamond = (n) =>{
+  if (n <= 0 || n % 2 === 0) return null
+  str = ''
+  for (i = 0; i < n; i++){
+    let len = Math.abs((n-2*i-1)/2)
+    str += ' '.repeat(len)
+    str += `*`.repeat(n-2*len)
+    str += `\n`
+  }
+  return str
+}

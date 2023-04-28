7f52161d7a61b309d25df0c3d1316ad77830caf4
johnhealycodes <johndhealy1216@gmail.com>
2023-04-27 19:20:26 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -413,3 +413,17 @@ function findNextSquare(sq) {
     return -1;
   }
 }
+
+// In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or"
+//(hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true,
+//false otherwise.
+
+const xor = (a, b) => {
+  if (a === true && b === true || a === false && b === false){
+    return false
+  } else if (a !== true && b !== true){
+    return true
+  } else {
+    return true
+  }
+}

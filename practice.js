924de29d07b3194a294d6c85750df3dfd57c373c
johnhealycodes <johndhealy1216@gmail.com>
2023-05-02 21:24:37 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -448,3 +448,15 @@ function reverseLetter(str) {
 // Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
 
 const replace = s => (s.split('').map(i => 'aeiou'.includes(i.toLowerCase()) ? '!' : i)).join('');
+
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10
+
+const multiTable = (number) => {
+  let table = '';
+  
+  for(let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+  }
+
+  return table;
+}

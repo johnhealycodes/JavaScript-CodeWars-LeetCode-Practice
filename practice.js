f7286b210c78712072a78a0da14701fc695541c8
johnhealycodes <johndhealy1216@gmail.com>
2023-05-13 22:51:21 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -487,3 +487,16 @@ const doubled = (x) => x.map(i => i*2)
 //If you get an empty array/list, return an empty array/list.
 
 const removeSmallest = (num) => num.filter((n, i) => i !== num.indexOf(Math.min(...num)))
+
+// In this kata you get the start number and the end number of a region and should return the count of all 
+// numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+const dontGiveMeFive = (start, end) => {
+  let result = 0
+  for (i = start; i <= end; i++){
+    if (!i.toString().includes("5")){
+      result++      
+    }
+  }
+  return result
+}

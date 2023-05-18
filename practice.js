65235f9cc17ea7b0104c9ef3513b12c6416c034c
johnhealycodes <johndhealy1216@gmail.com>
2023-05-17 21:16:35 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -515,3 +515,14 @@ var summation = function (num) {
   }
   return a.reduce((a, c) => a + c)
 }
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it.
+//If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+const getSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
